90e65b5c2eba0513aec70090f87c5d2d20e3b782
Pierre-Henry Soria <hi@ph7.me>
2018-01-20 15:15:27 +0000

    Store column names into constants

---
+++ src/Parser/DbProductColumns.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Parser;
+
+class DbProductColumns
+{
+    public const PRODUCT_NAME = 'name';
+    public const IMAGE_URL = 'image';
+    public const CALORIES = 'calories';
+    public const IS_ORGANIC = 'isOrganic';
+    public const IS_HEALTHY = 'isHealthy';
+    public const VITAMINS = 'vitamin';
+    public const FAT = 'fat';
+    public const SATURATED_FATS = 'saturated fats';
+    public const PROTEIN = 'Protein';
+    public const SUGAR = 'sugar';
+    public const SALT = 'salt';
+    public const CARBOHYDRATE = 'carbohydrate';
+    public const DIETARY_FIBER = 'dietary_fiber';
+    public const ALCOHOL = 'alcohol';
+}
