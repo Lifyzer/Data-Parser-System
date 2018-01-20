7a58081288aea21db064503e38dc1f16eaaf1ced
Pierre-Henry Soria <hi@ph7.me>
2018-01-20 02:42:13 +0000

    Add CSVFile tests

---
+++ tests/Parser/CsvFileTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hi@ph7.me>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Tests\Parser;
+
+use Lifyzer\Parser\CsvFile;
+use PHPUnit\Framework\TestCase;
+
+class CsvFileTest extends TestCase
+{
+    public function testValidValue(): void
+    {
+        $file = new CsvFile('my-great-file.csv');
+
+        $this->assertSame('my-great-file.csv', $file->getValue());
+    }
+
+    /**
+     * @dataProvider invalidFilenames
+     * @expectedException \Lifyzer\Parser\Exception\InvalidFileException
+     * @expectedExceptionCode \Lifyzer\Parser\Exception\InvalidFileException::WRONG_EXTENSION
+     */
+    public function testWrongExtension(string $filename): void
+    {
+        new CsvFile($filename);
+    }
+
+    /**
+     * @expectedException \Lifyzer\Parser\Exception\InvalidFileException
+     * @expectedExceptionCode \Lifyzer\Parser\Exception\InvalidFileException::TOO_SHORT
+     */
+    public function testTooShortFilename(): void
+    {
+        new CsvFile('/var/www/html/a.csv');
+    }
+
+    public function invalidFilenames(): array
+    {
+        return [
+            ['blabla.doc'],
+            ['myfile.CSV'],
+            ['myfile']
+        ];
+    }
+}
