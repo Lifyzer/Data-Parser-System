9ddf3ccd25297a41ff51a2da0a5cd3d6c04bb347
Pierre-Henry Soria <hi@ph7.me>
2018-01-21 23:04:09 +0000

    Update tests to match with new data fixtures

--- tests/Parser/ConverterTest.php
+++ tests/Parser/ConverterTest.php
@@ -20,36 +20,35 @@ class ConverterTest extends TestCase
     {
         $converter = $this->initializeConversion();
 
-        $this->assertSame([
-            'name' => 'NuutellaaPH',
-            'countries_en' => 'sugar',
-            'ingredients_text' => ' chocolate butter',
-            'allergens_en' => ' hazelnut',
-            'additives_en' => '30',
-            'sugar' => '300',
-            'cholesterol_100g' => '200',
-            'saturated-fat_100g' => '',
-            'fiber_100g' => '',
-            'proteins_100g' => '',
-            'casein_100g' => '',
-            'salt' => '',
-            'sodium_100g' => '',
-            'alcohol' => '',
-            'vitamin-a_100g' => '',
-            'vitamin-c_100g' => '',
-            'vitamin-d_100g' => '',
-            'vitamin-e_100g' => '',
-            'vitamin-k_100g' => '',
-            'calcium_100g' => '',
-            'magnesium_100g' => '',
-            'iron_100g' => '',
-            'zinc_100g' => '',
-            'cacao_100g' => '',
-            'fruits-vegetables-nuts_100g' => '',
-            'caffeine_100g' => '',
-            'isHealthy' => '0'
-        ],
-        $converter->asArray()[1]);
+        $this->assertSame(
+            [
+                1 => [
+                    'name' => 'Marmite yeast extract',
+                    'ingredients' => 'Yeast extract, salt, vegetable extract, niacin, thiamin, spice extracts (contains _celery_), riboflavin, folic acid, vitamin B12.',
+                    'image' => 'https://static.openfoodfacts.org/images/products/50184385/front_en.9.200.jpg',
+                    'saturatedFats' => '0',
+                    'sugar' => '1',
+                    'dietaryFiber' => '3.5',
+                    'protein' => '39',
+                    'salt' => '9.906',
+                    'alcohol' => '',
+                    'isHealthy' => '1'
+                ],
+                2 => [
+                    'name' => 'Yeast Extract',
+                    'ingredients' => 'Yeast extract, salt, carrot and onion extract, spice extracts, enriched with nicotinamide (niacin), thiamin hydrochloride, riboflavin and cyanocobalamin (vitamin b12).',
+                    'image' => '',
+                    'saturatedFats' => '',
+                    'sugar' => '',
+                    'dietaryFiber' => '',
+                    'protein' => '50',
+                    'salt' => '12.7',
+                    'alcohol' => '',
+                    'isHealthy' => '1',
+                ]
+            ],
+            $converter->asArray()
+        );
     }
 
     public function testXmlConverted(): void
@@ -59,92 +58,29 @@ class ConverterTest extends TestCase
         $expected = <<<'XML'
 <?xml version="1.0"?>
 <csv>
-  <record offset="0">
-    <field name="name">product_name</field>
-    <field name="countries_en">ingredients_text</field>
-    <field name="ingredients_text">proteins_100g</field>
-    <field name="allergens_en">sugars_100g</field>
-    <field name="additives_en">cacao_100g</field>
-    <field name="sugar"></field>
-    <field name="cholesterol_100g"></field>
-    <field name="saturated-fat_100g"></field>
-    <field name="fiber_100g"></field>
-    <field name="proteins_100g"></field>
-    <field name="casein_100g"></field>
-    <field name="salt"></field>
-    <field name="sodium_100g"></field>
-    <field name="alcohol"></field>
-    <field name="vitamin-a_100g"></field>
-    <field name="vitamin-c_100g"></field>
-    <field name="vitamin-d_100g"></field>
-    <field name="vitamin-e_100g"></field>
-    <field name="vitamin-k_100g"></field>
-    <field name="calcium_100g"></field>
-    <field name="magnesium_100g"></field>
-    <field name="iron_100g"></field>
-    <field name="zinc_100g"></field>
-    <field name="cacao_100g"></field>
-    <field name="fruits-vegetables-nuts_100g"></field>
-    <field name="caffeine_100g"></field>
-    <field name="isHealthy">0</field>
-  </record>
   <record offset="1">
-    <field name="name">NuutellaaPH</field>
-    <field name="countries_en">sugar</field>
-    <field name="ingredients_text"> chocolate butter</field>
-    <field name="allergens_en"> hazelnut</field>
-    <field name="additives_en">30</field>
-    <field name="sugar">300</field>
-    <field name="cholesterol_100g">200</field>
-    <field name="saturated-fat_100g"></field>
-    <field name="fiber_100g"></field>
-    <field name="proteins_100g"></field>
-    <field name="casein_100g"></field>
-    <field name="salt"></field>
-    <field name="sodium_100g"></field>
+    <field name="name">Marmite yeast extract</field>
+    <field name="ingredients">Yeast extract, salt, vegetable extract, niacin, thiamin, spice extracts (contains _celery_), riboflavin, folic acid, vitamin B12.</field>
+    <field name="image">https://static.openfoodfacts.org/images/products/50184385/front_en.9.200.jpg</field>
+    <field name="saturatedFats">0</field>
+    <field name="sugar">1</field>
+    <field name="dietaryFiber">3.5</field>
+    <field name="protein">39</field>
+    <field name="salt">9.906</field>
     <field name="alcohol"></field>
-    <field name="vitamin-a_100g"></field>
-    <field name="vitamin-c_100g"></field>
-    <field name="vitamin-d_100g"></field>
-    <field name="vitamin-e_100g"></field>
-    <field name="vitamin-k_100g"></field>
-    <field name="calcium_100g"></field>
-    <field name="magnesium_100g"></field>
-    <field name="iron_100g"></field>
-    <field name="zinc_100g"></field>
-    <field name="cacao_100g"></field>
-    <field name="fruits-vegetables-nuts_100g"></field>
-    <field name="caffeine_100g"></field>
-    <field name="isHealthy">0</field>
+    <field name="isHealthy">1</field>
   </record>
   <record offset="2">
-    <field name="name">Bonne MAman</field>
-    <field name="countries_en"> Strawberry</field>
-    <field name="ingredients_text"> sugar</field>
-    <field name="allergens_en"> jam</field>
-    <field name="additives_en"> strawberry</field>
-    <field name="sugar">0</field>
-    <field name="cholesterol_100g">200</field>
-    <field name="saturated-fat_100g">0</field>
-    <field name="fiber_100g"></field>
-    <field name="proteins_100g"></field>
-    <field name="casein_100g"></field>
-    <field name="salt"></field>
-    <field name="sodium_100g"></field>
+    <field name="name">Yeast Extract</field>
+    <field name="ingredients">Yeast extract, salt, carrot and onion extract, spice extracts, enriched with nicotinamide (niacin), thiamin hydrochloride, riboflavin and cyanocobalamin (vitamin b12).</field>
+    <field name="image"></field>
+    <field name="saturatedFats"></field>
+    <field name="sugar"></field>
+    <field name="dietaryFiber"></field>
+    <field name="protein">50</field>
+    <field name="salt">12.7</field>
     <field name="alcohol"></field>
-    <field name="vitamin-a_100g"></field>
-    <field name="vitamin-c_100g"></field>
-    <field name="vitamin-d_100g"></field>
-    <field name="vitamin-e_100g"></field>
-    <field name="vitamin-k_100g"></field>
-    <field name="calcium_100g"></field>
-    <field name="magnesium_100g"></field>
-    <field name="iron_100g"></field>
-    <field name="zinc_100g"></field>
-    <field name="cacao_100g"></field>
-    <field name="fruits-vegetables-nuts_100g"></field>
-    <field name="caffeine_100g"></field>
-    <field name="isHealthy">0</field>
+    <field name="isHealthy">1</field>
   </record>
 </csv>
 
