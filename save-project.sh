762c8f61e7909e328260523a06c6ede41f99fb37
Pierre-Henry Soria <hi@ph7.me>
2018-02-05 22:21:36 +0000

    Allow to directly backup the project to the three repos

---
+++ save-project.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Save the git project to a specific repo (e.g. github, bitbucket, ...)
+function save-project() {
+    git remote rm origin
+    git remote add origin $1
+    git push
+}
+
+save-project git@github.com:Lifyzer/Data-Parser-System.git
+save-project git@gitlab.com:pH-7/lifyzer-data-parser-system.git
+save-project git@bitbucket.org:pH_7/lifyzer-data-parser-system.git
