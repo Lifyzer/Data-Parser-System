110869689820616d76be15014d9b6d35faa114bc
Pierre-Henry Soria <hi@ph7.me>
2018-02-08 20:43:58 +0000

    Add test to make sure asSQL()'s behaviour is correct

--- tests/Parser/ConverterTest.php
+++ tests/Parser/ConverterTest.php
@@ -109,6 +109,35 @@ CSV;
         $this->assertSame($expected, $converter->asCsv());
     }
 
+    public function testSqlConverted(): void
+    {
+        $converter = $this->initializeConversion();
+
+        $expected = <<<'SQL'
+        CREATE TABLE products (
+            name varchar(255) NOT NULL,
+            ingredients text NOT NULL,
+            image varchar(255) NOT NULL,
+            saturatedFats float NOT NULL,
+            carbohydrate float NOT NULL,
+            sugar float NOT NULL,
+            dietaryFiber float NOT NULL,
+            protein float NOT NULL,
+            salt float NOT NULL,
+            sodium float NOT NULL,
+            alcohol float NOT NULL,
+            isHealthy enum('1','0') NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO products (name, ingredients, image, saturatedFats, carbohydrate, sugar, dietaryFiber, protein, salt, sodium, alcohol, isHealthy)
+VALUES ('Marmite yeast extract', 'Yeast extract, salt, vegetable extract, niacin, thiamin, spice extracts (contains _celery_), riboflavin, folic acid, vitamin B12.', 'https://static.openfoodfacts.org/images/products/50184385/front_en.9.200.jpg', '0', '24', '1', '3.5', '39', '9.906', '3.9', '', '1');
+INSERT INTO products (name, ingredients, image, saturatedFats, carbohydrate, sugar, dietaryFiber, protein, salt, sodium, alcohol, isHealthy)
+VALUES ('Yeast Extract', 'Yeast extract, salt, carrot and onion extract, spice extracts, enriched with nicotinamide (niacin), thiamin hydrochloride, riboflavin and cyanocobalamin (vitamin b12).', '', '', '0', '', '', '50', '12.7', '5', '', '1');
+
+SQL;
+        $this->assertSame($expected, $converter->asSql());
+    }
+
     public function testProductDataWithoutName(): void
     {
         $fullPathFile = $this->getFixtureCsvDataWithoutProductName();
